670fc3fd9138f361df44da26c992590e8d56600a
isaque araujo nogueira <isaquean2@gmail.com>
2021-10-18 14:33:20 -0300

    removed files

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const routes = express.Router()
-
-// nomeia as rotas; envia variaveis para o html por meio do EJS
-routes.get('/', (req, res) => res.render("index"))
-routes.post('/', (req, res) => {console.log(req.body)})
-routes.get('/NewTransaction', (req, res) => res.render("NewTransaction"))
-
-module.exports = routes;
